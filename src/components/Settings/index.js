0c6f24068acf7d3f1a917b69775fea66f4a13c87
Shrihari Prakash <shrihariprakash@gmail.com>
2021-08-07 16:54:09 +0530

    - Added settings list.

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,16 +1,59 @@
+import { LockOutlined, SkinOutlined, UserOutlined } from "@ant-design/icons";
 import { useHistory } from "react-router-dom";
+import { getLoggedInUser } from "../../utils/auth";
+import { StyledList } from "../common/List/styles";
 import PageHeader from "../common/PageHeader";
 import { SettingsWrapper } from "./styles";
 
 const Settings = () => {
   const history = useHistory();
   const onBack = () => history.push("/conversations");
+  const settingsList = [
+    {
+      id: 1,
+      icon: <UserOutlined />,
+      title: "My Profile",
+      description:
+        "Logged in as " +
+        getLoggedInUser().name.givenName +
+        " " +
+        getLoggedInUser().name.familyName,
+      action: () => null,
+    },
+    {
+      id: 2,
+      icon: <SkinOutlined />,
+      title: "Themes",
+      description: null,
+      action: () => null,
+    },
+    {
+      id: 3,
+      icon: <LockOutlined />,
+      title: "Privacy",
+      description: null,
+      action: () => null,
+    },
+  ];
+
   return (
     <SettingsWrapper>
       <PageHeader onBack={onBack}>
         Settings
         <div className="dummy"></div>
       </PageHeader>
+      <StyledList
+        dataSource={settingsList}
+        renderItem={(setting) => (
+          <StyledList.Item key={setting.id}>
+            <StyledList.Item.Meta
+              avatar={setting.icon}
+              title={setting.title}
+              description={setting.description}
+            />
+          </StyledList.Item>
+        )}
+      ></StyledList>
     </SettingsWrapper>
   );
 };
